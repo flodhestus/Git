f9d4b1726c7466a410fc14eb6dde09c50469d0c6
eric flodin <eric.flodin@aol.com>
2021-10-12 11:05:13 +0200

    Greeter

---
+++ Desktop/repos/Git/Greeter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    class Greeter
+    {
+
+        public void SayHello()
+        {
+            Console.WriteLine("Hello!");
+
+
+        }
+
+
+
+
+    }
+}
